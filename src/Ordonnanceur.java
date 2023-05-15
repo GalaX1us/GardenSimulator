50cf57465af76b8e65cb430eccc9aec07d869b0d
Aurélien BOISSOT <aurelien.boissot@gmail.com>
2023-05-15 10:50:57 +0200

    modif ordonnanceur

--- src/Ordonnanceur.java
+++ src/Ordonnanceur.java
@@ -3,11 +3,28 @@ import java.util.Observable;
 public class Ordonnanceur extends Observable implements Runnable{
 
     private Runnable[] tab;
+    private static Ordonnanceur o;
+
+    public static Ordonnanceur getOrdonnanceur() {
+        if(o==null) {
+            o = new Ordonnanceur();
+        }
+        return o;
+    }
 
     @Override
     public void run() {
-        for(Runnable r : tab) {
-            r.run();
+        while(true) {
+            for(Runnable r : tab) {
+                r.run();
+            }
+            setChanged();
+            notifyObservers();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
     
