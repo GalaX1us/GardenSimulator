6fc6bba9ccb5dcd6212ecbbf141c985bf6e81589
Nathan BONADA <nathan.bonada@gmail.com>
2023-05-28 21:35:24 +0200

    changement croissance

--- src/Legumes/Aubergine.java
+++ src/Legumes/Aubergine.java
@@ -10,10 +10,11 @@ public class Aubergine extends Legume {
 
     @Override
     public void calculCroissance(float humidite, float temperature) {
-        if(this.croissance<1){
-            croissance += ((humidite - (0.5f-temperature)*2)+1)/50;
+        if(!this.harvestable){
+            float offset = Math.max(((humidite - (0.5f-temperature)*2)+1)/200, 0f);
+            croissance += offset;
         }
-        if (this.croissance>1) this.croissance = 1;
+        if (this.croissance>=1) this.harvestable = true;
     }
     
 }
--- src/Legumes/Salade.java
+++ src/Legumes/Salade.java
@@ -12,7 +12,7 @@ public class Salade extends Legume {
     @Override
     public void calculCroissance(float humidite, float temperature) {
         if(!this.harvestable){
-            float offset = Math.max(((humidite - (0.5f-temperature)*2)+1)/50, 0f);
+            float offset = Math.max(((humidite - (0.5f-temperature)*2)+1)/200, 0f);
             croissance += offset;
         }
         if (this.croissance>=1) this.harvestable = true;
