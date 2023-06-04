52dd0eb9d750c9219a076a9605462f38688b294e
nathan <nathan.bonada@gmail.com>
2023-06-04 18:02:10 +0200

    fix arrosoir

--- src/Vue/Vue.java
+++ src/Vue/Vue.java
@@ -131,7 +131,7 @@ public class Vue extends JFrame implements Observer {
                                 Potager.ajoutArgent(-casePotager.getPrix());
                             }
                         }
-                        else if(Potager.getSelection().equals("arrosoir") && casePotager.getContientLegume()){
+                        else if(Potager.getSelection().equals("arrosoir") && casePotager.getContientLegume() && !P.getParcelle(ii, jj).getLegume().isHarvestable()){
                             P.getParcelle(ii, jj).arroser();
                         }
                         else if(Potager.getSelection().equals("engrais") && casePotager.getContientLegume()){
